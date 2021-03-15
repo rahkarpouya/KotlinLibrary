bbed01eb59cd0f4be84d295e64068116da223bc6
Reza Darvishian <reza.da9@gmail.com>
2021-03-15 09:58:54 +0330

    debug

--- LibKot/src/main/java/com/ali74/libkot/slider/SlideShow.kt
+++ LibKot/src/main/java/com/ali74/libkot/slider/SlideShow.kt
@@ -2,9 +2,9 @@ package com.ali74.libkot.slider
 
 
 data class SlideShow(
-    val ImageUrl: String,
-    val CategoryID: Int,
-    val ProductID: Int,
+    val ImageUrl: String?,
+    val CategoryID: Int?,
+    val ProductID: Int?,
     val RedirectLink: String?,
     val Any: String? = null
 )
